37f9c01bbe79a11842678bb04d642419a7012ff4
avirameldar <aviram.eldar@gmail.com>
2022-08-21 19:44:29 +0300

    make the time run

--- stop-watch/step-3/stop-watch.js
+++ stop-watch/step-3/stop-watch.js
@@ -1,13 +1,36 @@
 const stopWatchView = document.querySelector('#stop-watch-view');
 
-let hundredth = 44;
-let seconds = 34;
-let minutes = 12;
+let hundredth = 0;
+let seconds = 0;
+let minutes = 0;
+
 
 
 function displayTime() {
+    let secondString = seconds;
+    if (seconds < 10) {
+        secondString = '0' + seconds;
+    }
+    let minutesString = minutes;
+    if (minutes < 10) {
+        minutesString = '0' + minutes;
+    }
+
     // stopWatchView.innerHTML = minutes + ':' + seconds + ':' + hundredth
     stopWatchView.innerHTML = `${minutes}:${seconds}:${hundredth}`;
 };
 
-displayTime();
+
+function handleTimeChange() {
+    hundredth++;
+    if (hundredth > 99) {
+        hundredth = 0;
+        seconds++;
+        if (seconds > 59) {
+            seconds = 0;
+            minutes++;
+        }
+    }
+    displayTime();
+}
+setInterval(handleTimeChange, 10);
