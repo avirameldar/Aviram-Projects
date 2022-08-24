d539e428e9187c5614be49393fa340886f58dcb3
avirameldar <aviram.eldar@gmail.com>
2022-08-24 20:51:54 +0300

    step 7 with bug

---
+++ Timer/step-7/timers.js
@@ -0,0 +1,34 @@
+function Timer(initialTime) {
+    let timerElement;
+
+    this.initialTime = initialTime;
+
+    this.deleteTimer = function () {
+        console.log('you want to delete somthing', timerElement);
+        timerElement.remove();
+    }
+
+    this.createTimer = function () {
+        const timersContainers = document.querySelector('#timers-container');
+
+        this.timerElement = document.createElement('div');
+
+        const startButton = document.createElement('button')
+        startButton.innerText = 'Start';
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.addEventListener('click', this.deleteTimer)
+
+        timerElement.innerText = 'this.initialTime';
+        timerElement.appendChild(startButton);
+        timerElement.appendChild(deleteButton);
+        timerElement.appendChild(timerElement);
+
+
+        // timersContainers.innerHTML += '<div>new timer</div>';
+        // console.log('create timer');
+    }
+}
+
+export default Timer;
