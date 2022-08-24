f900295daf7b89f9374c9b58476a2d405293164f
avirameldar <aviram.eldar@gmail.com>
2022-08-24 19:51:29 +0300

    add  button

---
+++ Timer/step-4/app.js
@@ -0,0 +1,12 @@
+import Timer from './timers.js';
+
+// const t1 = new Timer(50);
+// console.log('timer', t1);
+
+document.querySelector('#create-timer').addEventListener('click', addTimer)
+const timerInput = document.querySelector('#time-input')
+function addTimer() {
+    console.log('add new timer');
+    const initialTime = document.querySelector('#time-input').value;
+    console.log('value from infut', initialTime);
+}
