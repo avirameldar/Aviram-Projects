efef5c441caf1c070e126e42550931c0a8a357db
avirameldar <aviram.eldar@gmail.com>
2022-08-24 18:16:09 +0300

    Timer Step02

--- Timer/Step1/timer.js
+++ Timer/Step1/timer.js
@@ -2,8 +2,10 @@ const fullName = {
     name: 'Aviram',
     Last_name: 'Eldar',
     fnmae: function () {
+        console.log('fname function');
+    },
+};
 
-    }
-}
+fullName.name.fname();
 
 export default fullName;
